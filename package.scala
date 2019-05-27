ba3814796b0a4a880820fc5e0b6a940badec2cd0
Jesús López-González <jesus.lopez@hablapps.com>
2019-05-27 09:37:38 +0200

    Using own optic structures instead of aliases

--- package.scala
+++ package.scala
@@ -2,12 +2,6 @@ package dev
 
 package object `habla` {
 
-  type Getter[S, A] = S => A
-
-  type Affine[S, A] = S => Option[A]
-
-  type Fold[S, A] = S => List[A]
-
   type Org = List[Department]
 }
 
